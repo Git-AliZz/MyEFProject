227815977881909fe3613a97b96cf8eeed257ed6
ali <alizadeh-ali1@gmail.com>
2021-10-29 22:49:41 +0330

    CRUD

--- MyEFProject_DataAccess/Migrations/20211029091752_InsertData.cs
+++ MyEFProject_DataAccess/Migrations/20211029091752_InsertData.cs
@@ -6,7 +6,11 @@ namespace MyEFProject_DataAccess.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("Insert into Categories Values('Category 1')");
+            migrationBuilder.Sql("Insert into Categories Values('Category 2')");
+            migrationBuilder.Sql("Insert into Categories Values('Category 3')");
+            migrationBuilder.Sql("Insert into Categories Values('Category 4')");
+            migrationBuilder.Sql("Insert into Categories Values('Category 5')");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
