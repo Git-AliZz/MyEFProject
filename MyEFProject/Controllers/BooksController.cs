394f151c25b4ee8663ac44dd73ce66deb06b9355
ali <alizadeh-ali1@gmail.com>
2021-11-06 23:09:27 +0330

    Add BooksController

---
+++ MyEFProject/Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyEFProject_DataAccess.Data;
+using MyEFProject_Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEFProject.Controllers
+{
+    public class BooksController : Controller
+    {
+        private ApplicationDbContext _db;
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var books = _db.Books.ToList();
+
+            return View(books);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            BookVM bookVM = new BookVM();
+            bookVM.PublisherList = _db.Publishers.Select(P => new SelectListItem()
+            {
+                Text = P.Name,
+                Value = P.Publisher_Id.ToString()
+            });
+
+            if (id == null)
+            {
+                return View(bookVM);
+            }
+
+            bookVM.Book = _db.Books.Find(id);
+            if (bookVM.Book == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookVM);
+        }
+    }
+}
---
+++ MyEFProject_Model/ViewModel/BookVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyEFProject_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEFProject_Model.ViewModel
+{
+    public class BookVM
+    {
+        public Book Book { get; set; }
+        public IEnumerable<SelectListItem> PublisherList { get; set; }
+    }
+}
